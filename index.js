6e4dc1422200bc57b872862df9e0cbeb5c35f19a
Hatemii <hatem.xhezairi96@gmail.com>
2021-03-02 21:32:52 +0100

    GET/:id

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const express = require("express")
 const app = express()
 
+
+// ############################ FIRST PART (GET)
+
+// GET REQUESTS
 app.get("/", (req, res) => {
     res.send("Hello")
 })
 
-
 app.get("/api/courses", (req, res) => {
-    res.send("Second way " + [1, 2, 3, 4])
+    res.send(courses)
 })
 
 
-app.listen(3000, () => console.log("Port started on port 3000..."))
+
+// ############################ SECOND PART (GET/:id)
+
+// OBJECT
+const courses = [
+    { id: 1, name: "course 1" },
+    { id: 2, name: "course 2" },
+    { id: 3, name: "course 3" },
+]
+
+// course with given ID
+app.get("/api/courses/:id", (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id)) // this return String so we need to convert it 
+
+    if (!course) {
+        res.status(404).send("The course with the given ID was not found!")
+    } else {
+        res.send(course)
+    }
+
+})
+
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server started on PORT ${port}`)) 
